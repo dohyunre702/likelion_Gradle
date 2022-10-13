42a3a39a571d3c372e9c8d85a0541b9c86a8a849
dohyu <dohyunre702@gmail.com>
2022-10-13 14:58:51 +0900

    middle commit 1. HospitalParser 2. HospitalParserTest -subdivision까지

--- src/main/java/com/line/domain/Hospital.java
+++ src/main/java/com/line/domain/Hospital.java
@@ -3,19 +3,71 @@ package com.line.domain;
 public class Hospital {
     private String id;
     private String address;
-    private String district;
+    private String district; //address에서 추출
     private String category;
     private Integer emergencyRoom;
     private String name;
     private String subdivision;
+
+    //생성자 (id, address)
+    public Hospital(String id, String address) {
+        this.id = replaceAllQuot(id);
+        this.address = replaceAllQuot(address);
+    }
+
+    //테스트용 line1
+    String line1 = "\"A1120837\",\"서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)\",\"C\",\"의원\",\"G099\",\"응급의료기관 이외\",\"2\",\"외과: 상시진료 내과는 당분간 휴진\",\"서울시 송파구 문정동 장지동 법조단지 위례 가락동 가락시장역 위치 삼성서울병원 외래교수 출신 구강외과 전문의 진료 진료과목 - 임플란트 치조골 뼈이식 수술 매복 사랑니 발치 턱관절 악관절 질환의 치료 교정 치료 및 기타 보존 보철(크라운 브릿지 인레이) 신경치료\",\"방이역 1번출구 바로옆 굿모닝 신한증권 뒷건물\",\"가산기대찬의원\",\"02-6267-2580\",\"02-920-5374\",\"1930\",\"1930\",\"1930\",\"1930\",\"1930\",\"1500\",\"1500\",\"1500\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"1000\",\"1000\",\"085\",\"11\",\"126.88412249700781\",\"37.4803938036867\",\"2022-04-07 14:55:00.0\"";
+    String line2 = "B1100027","서울특별시 강남구 봉은사로 612 (삼성동)","E","한방병원","G099","응급의료기관 이외","2","진료과목마다 진료시간 다릅니다. 전화 확인 후 방문해주세요.","","봉은사역 5번 출구 삼성1동주민센터 옆","(의)가산의료재단광동한방병원","02-2222-4888","","1800","1800","1800","1800","1800","1500","","","0900","0900","0900","0900
+
+    public Hospital(String id, String address, String category, Integer emergencyRoom, String name, String subdivision) {
+        this.id = id;
+        this.address = address;
+        String[] splitted = this.address.split(" ");
+        this.district = String.format("%s %s",splitted[0], splitted[1]);
+        this.category = category;
+        this.emergencyRoom = emergencyRoom;
+        this.name = name;
+        this.subdivision = subdivision;
+    }
+
+
+    private String replaceAllQuot(String str) {
+        return str.replaceAll("\"","");
+    }
 
 
     //생성자를 통해서만 초기화 가능
     public Hospital(String id) {
         this.id = id.replaceAll("\"", "");
     }
+
+    // getter
     //id를 뽑아간다(외부에서 접근)
     public String getId() {
         return id;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDistrict() {
+        String[] splitted = this.address.split(" ");
+        return String.format("%s %s",splitted[0], splitted[1]);
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Integer getEmergencyRoom() {
+        return emergencyRoom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubdivision() {
+        return subdivision;
     }
 }
--- src/main/java/com/line/parser/HospitalParser.java
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -5,10 +5,38 @@ import com.line.domain.Hospital;
 //인터페이스 Parser의 구현체
 public class HospitalParser implements Parser<Hospital>{
 
+    private String getSubdivision(String name){
+        String[] subdivisions = {"소아과", "피부과", "성형외과", "정형외과", "산부인과", "안과", "가정의학과", "비뇨기과", "치과", "내과", "외과"};
+
+        for (String subdivision : subdivisions){
+            if (name.contains(subdivision)) {
+                return subdivision;
+            }
+        }
+        return "";
+
+    }
+
+
     //string을 받아서 hospital 리턴
     @Override
     public Hospital parse(String str) {
+        str = str.replaceAll("\"", "");
         String[] splitted = str.split(",");
-        return new Hospital(splitted[0]);
+        // address;
+        // district;
+        // category;
+        // emergencyRoom;
+        // name;
+        // subdivision;
+
+        //subdivision 파싱해서 넣기
+        String name = splitted[10];
+        String subdivision = getSubdivision(name);
+
+        return new Hospital(splitted[0], splitted[1], splitted[2], Integer.parseInt(splitted[6]), name,subdivision);
     }
+
+
+
 }
